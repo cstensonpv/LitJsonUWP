06e954e6d0826ebec10c5098dbbc348d2c51c40f
Calle <cstenson@kth.se>
2018-01-27 11:29:39 +0100

    initial commit

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using LitJson;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Test h = new Test();
+            h.test = "test";
+            JsonMapper.ToJson(h);
+            Assert.IsFalse(false);
+            Assert.IsTrue(h.test.Equals("test"));
+        }
+
+        public class Test
+        {
+            public string test;
+        }
+    }
+}
